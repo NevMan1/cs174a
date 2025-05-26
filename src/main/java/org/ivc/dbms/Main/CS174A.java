229d51f3ac0c1ea999c89e85dde27c8b6be726c1
Nevin Manimaran <nevin@ucsb.edu>
2025-05-26 11:54:04 -0700

    Initial commit with existing code

---
+++ src/main/java/org/ivc/dbms/Main/CS174A.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.ivc.dbms.Main;
+
+/**
+ *
+ * @author legitbrunogmail.com
+ */
+public class CS174A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
